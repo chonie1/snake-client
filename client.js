5a1f48aa547d9b732150f062f24da9dcfff86d87
Connie Ho <connie3@ualberta.ca>
2021-02-09 21:29:17 +0000

    seperating client module

---
+++ client.js
@@ -0,0 +1,25 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+
+const connect = function() {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+  
+  //message handling on the server
+  conn.on('data',(data) => {
+    console.log(data.toString());
+  });
+
+  return conn;
+};
+
+module.exports = {
+  connect
+};
--- play.js
+++ play.js
@@ -1,23 +1,3 @@
-const net = require('net');
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8');
-  
-  //message handling on the server
-  conn.on('data',(data) => {
-    console.log(data.toString());
-  });
-
-  return conn;
-};
-
+const {connect} = require('./client');
 console.log('Connecting ...');
 connect();
