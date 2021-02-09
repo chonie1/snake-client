71cfcac482adb43e301373389f4f38437a22ff41
Connie Ho <connie3@ualberta.ca>
2021-02-09 23:32:19 +0000

    updating snake with name

--- client.js
+++ client.js
@@ -12,10 +12,20 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
   
+  conn.on('connect',()=>{
+    console.log('Successfully connected to game server');
+    conn.write('Name: CH');
+  });
+
+  
+
+  
   //message handling on the server
   conn.on('data',(data) => {
     console.log(data.toString());
   });
+
+
 
   return conn;
 };
